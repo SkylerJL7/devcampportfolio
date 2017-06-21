933cb72556b03101d54067753caf09a85f82516d
Alpine Devcamp <alpine@devcamp.com>
2017-06-21 14:36:32 +0000

    Installed devise gem

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,14 +2,18 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+  
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
 
   def new
-    @portfolio_item = Portfolio.new
-  end
-    
-    def create
+  @portfolio_item = Portfolio.new
+  3.times { @portfolio_item.technologies.build }
+  end  
 
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
 
     respond_to do |format|
       if @portfolio_item.save
@@ -19,6 +23,25 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+  
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+  
+  def update 
+    @portfolio_item = Portfolio.find(params[:id])
+    
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+  
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
 
  def destroy
     # Perform the lookup
@@ -29,8 +52,8 @@ class PortfoliosController < ApplicationController
 
     # Redirect
     respond_to do |format|
-      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+      format.html { redirect_to portfolios_url, notice: 'Record was removed' }
     end
   end
-  
+end
 end
