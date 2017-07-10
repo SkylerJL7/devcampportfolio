1da47971d00608bc30bff1b0913df8a2ed34e90e
Alpine Devcamp <alpine@devcamp.com>
2017-07-10 17:46:39 +0000

    Did Lots of Stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,19 +4,5 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
-  
-  before_action :set_copyright
-  
-  def set_copyright
-    @copyright = BroDogMate::Renderer.copyright 'Skyler Jackson', 'All rights reserved'
-  end
-end
-
-module BroDogMate
-  class Renderer
-    def self.copyright name, msg
-      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
-    end
-  end
 end
  
